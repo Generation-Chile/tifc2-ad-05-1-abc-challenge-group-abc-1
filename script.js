462f114539ef0ab94ffb6e7fc34a12a605727b05
knma5 <keila.martinez@genstudents.org>
2025-01-08 08:36:18 -0300

    js-h5

---
+++ script.js
@@ -0,0 +1,17 @@
+/*•	Cuando alguien haga clic en cualquier etiqueta h5, 
+haga que cambie a 3 colores aleatorios repetibles. 
+Verde, Azul y Rojo.*/
+
+const h5Elements = document.querySelectorAll('h5');
+
+function getRandomColor() {
+  const colors = ['green', 'blue', 'red'];
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+}
+
+h5Elements.forEach(h5 => {
+  h5.addEventListener('click', () => {
+    h5.style.color = getRandomColor();
+  });
+});
